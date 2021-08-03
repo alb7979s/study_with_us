33586a8f481b24db7ff65fa5557a4f6dff21eae9
김종혁 <alb7979s@naver.com>
2021-08-03 22:04:02 +0900

    feat: response 객체 정형화

---
+++ src/main/java/com/ssafy/study_with_us/util/response/ResponseMessage.java
@@ -0,0 +1,27 @@
+package com.ssafy.study_with_us.util.response;
+
+public class ResponseMessage {
+    public static final String CREATED_MEMBER = "회원 가입 성공";
+    public static final String LOGIN_SUCCESS = "로그인 성공";
+    public static final String LOGIN_FAIL = "로그인 실패";
+    public static final String READ_MEMBER = "회원 정보 조회 성공";
+    public static final String NOT_FOUND_MEMBER = "회원을 찾을 수 없습니다.";
+    public static final String PWD_MAIL_SUCCESS = "비밀번호 찾기 메일 전송 성공";
+    public static final String UPDATED_MEMBER = "회원 정보 수정 성공";
+    public static final String DELETED_MEMBER = "회원 탈퇴 성공";
+
+    public static final String CREATED_STUDY = "스터디 생성 성공";
+    public static final String UPDATED_STUDY = "스터디 수정 성공";
+    public static final String SEARCHED_STUDY = "스터디 검색 성공";
+    public static final String CREATED_STUDY_MEMBER = "스터디 회원 가입 성공";
+    public static final String DELETED_STUDY_MEMBER = "스터디 회원 탈퇴 성공";
+    public static final String SEARCHED_STUDY_THEMES = "스터디 테마 검색 성공";
+
+    public static final String CREATED_COMMENT = "댓글 생성 성공";
+    public static final String UPDATED_COMMENT = "댓글 수정 성공";
+    public static final String DELETED_COMMENT = "댓글 삭제 성공";
+    public static final String SEARCHED_COMMENTS = "댓글 검색 성공";
+
+    public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
+    public static final String DB_ERROR = "데이터베이스 에러";
+}
